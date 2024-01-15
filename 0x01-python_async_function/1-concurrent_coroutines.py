2cbdeb3646f4758a973de7f693826c20665aaae9
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-16 01:48:30 +0200

    Update 1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" asynchronous """
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ doc """
+    lst = []
+
+    for i in range(n):
+        lst.append(await wait_random(max_delay))
+
+    return sorted(lst)
