139d3a7a784b7e3aa1a85fbc2d397b34f89d57aa
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-18 18:04:06 +0200

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,14 +3,14 @@
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ doc """
     lst = []
 
-    for i in range(n):
-        lst.append(await wait_random(max_delay))
+    for _ in range(n):
+        lst.append(await task_wait_random(max_delay))
 
     return sorted(lst)
