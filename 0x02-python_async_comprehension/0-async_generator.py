a05c65a5244f0d221ac21266a25eb7188c9931c6
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-19 02:28:29 +0200

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ asynchronous """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ doc """
     for _ in range(10):
         await asyncio.sleep(1)
