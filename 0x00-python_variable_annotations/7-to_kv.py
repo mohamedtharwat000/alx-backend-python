43c9e957f2ac9aee6bd4a6389df71e854fef6a15
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 00:34:00 +0200

    Create 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Convert a string to a tuple."""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Convert a string to a tuple."""
+    return (k, v)
