c42d2ee4922748cb9aad93f7c28996aa74c6da6d
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 00:31:10 +0200

    Create 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Sum a list of floats and integers."""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """Sum a list of floats and integers."""
+    return sum(mxd_lst)
