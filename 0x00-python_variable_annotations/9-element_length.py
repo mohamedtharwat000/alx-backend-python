eddcde7c6bc0af1c8f835eea7dec4df3e0a93c07
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 00:46:36 +0200

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 
 """Annotate a function's parameters and return value."""
 
-from typing import Sequence, Tuple, List
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Sequence[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Annotate a function's parameters and return value."""
     return [(i, len(i)) for i in lst]
