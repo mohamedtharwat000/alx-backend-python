5b13506bb1f64d286a02d956af3d1a39e13da429
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 00:15:18 +0200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Floor a float and return the floor as an int."""
+
+
+def floor(n: float) -> int:
+    """Floor a float and return the floor as an int."""
+    return int(n)
